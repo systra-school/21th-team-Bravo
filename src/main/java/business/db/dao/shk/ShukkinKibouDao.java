f85bb809be939f21c659e05bbe4baf3c38dea5b4
Naoki Oota <hao123@a.vn>
2023-09-06 15:51:29 +0900

    出勤希望確認のテーブルが詰めるのではなく日別で表示されるようにしました。

--- src/main/java/business/db/dao/shk/ShukkinKibouDao.java
+++ src/main/java/business/db/dao/shk/ShukkinKibouDao.java
@@ -130,7 +130,7 @@ public class ShukkinKibouDao extends AbstractDao{
             strSql.append("MSHIFT.SYMBOL, ");
             strSql.append("YEAR_MONTH_DAY ");
             strSql.append("FROM ");
-            strSql.append("T_SHIFT TSHIFT RIGHT OUTER JOIN ");
+            strSql.append("T_SHIFT TSHIFT LEFT OUTER JOIN ");
             strSql.append("M_SHIFT MSHIFT ON ");
             
             //結合の条件指定
