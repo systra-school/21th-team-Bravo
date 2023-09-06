e9bb1a5dbc93765d3004676609ed941e12d632a4
KawakuboAzusa <a@a.jp>
2023-09-06 15:14:17 +0900

    ダイヤログの表示月を3ヶ月にしてます

--- src/main/java/action/mth/TsukibetsuShiftNyuuryokuSearchAction.java
+++ src/main/java/action/mth/TsukibetsuShiftNyuuryokuSearchAction.java
@@ -90,7 +90,7 @@ public class TsukibetsuShiftNyuuryokuSearchAction extends TsukibetsuShiftNyuuryo
         // セレクトボックスの取得
         ComboListUtilLogic comboListUtils = new ComboListUtilLogic();
         Map<String, String> shiftCmbMap = comboListUtils.getComboShift(true);
-        Map<String, String> yearMonthCmbMap = comboListUtils.getComboYearMonth(CommonUtils.getFisicalDay(CommonConstant.yearMonthNoSl), 3, ComboListUtilLogic.KBN_YEARMONTH_NEXT, false);
+        Map<String, String> yearMonthCmbMap = comboListUtils.getComboYearMonth(CommonUtils.getFisicalDay(CommonConstant.yearMonthNoSl), 2, ComboListUtilLogic.KBN_YEARMONTH_NEXT, false);
 
         if (CheckUtils.isEmpty(tsukibetsuShiftDtoMap)) {
             // データなし
